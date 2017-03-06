581a39fdc654397660a70a1df1a35003f2611997
akgoode <akgoode@gmail.com>
2017-03-06 12:29:08 -0500

    Include mime and path so that we can attach an extension name to the uploaded picture.
    AWS now stores it properly and can display it properly.

--- bin/aws-upload.js
+++ bin/aws-upload.js
@@ -5,19 +5,25 @@ require('dotenv').load();
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
 const fs = require('fs');
+const mime = require('mime');
+const path = require('path');
 
 let file = {
   path: process.argv[2],
   title: process.argv[3]
 };
 
+let mimeType = mime.lookup(file.path);
+let ext = path.extname(file.path);
+
 let stream = fs.createReadStream(file.path);
 
 let params = {
   ACL: 'public-read',
   Bucket: process.env.AWS_S3_BUCKET_NAME,
-  Key: file.title,
-  Body: stream
+  Key: `${file.title}${ext}`,
+  Body: stream,
+  ContentType: mimeType
 };
 
 s3.upload(params, (err, data) => {
